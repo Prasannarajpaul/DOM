a21538f78b16ba7a539ac166763d72484bfbf324
Prasannarajpaul <104220676+Prasannarajpaul@users.noreply.github.com>
2023-09-30 22:30:06 +0530

    gyro rem

--- Dice/script.js
+++ Dice/script.js
@@ -1,21 +1,2 @@
 var num = Math.floor(Math.random()*6)+1;
 document.querySelector(".dice1").innerHTML = num;
-
-// Create a new Gyro object
-var gyro = new Gyroscope();
-
-// Start listening for gyro events
-gyro.addEventListener("change", function(e) {
-
-  // Get the current rotation of the device
-  var rotation = e.rotation;
-
-  // Get the current angle of the device
-  var angle = rotation.alpha;
-
-  // Generate a random number between 1 and 6
-  var number = Math.floor(Math.random() * 6) + 1;
-
-  // Display the number on the screen
-  document.querySelector(".dice1").innerHTML = num;
-});
